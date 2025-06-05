635da1cb474090f8ff7292a633e868f1038d5276
talalalmrka <talalminfo@gmail.com>
2025-06-06 01:14:46 +0300

    Refactor ReadingTime component to simplify tooltip handling by removing unused computed properties and adjusting data attributes for improved clarity.

--- src/client/components/pageinfo/ReadingTime.vue
+++ src/client/components/pageinfo/ReadingTime.vue
@@ -6,18 +6,21 @@ import {
 } from '@vuepress/plugin-reading-time/client'
 import { useData } from '@theme/client/composables';
 const { themeLocale } = useData()
-const { readingTimeIcon, readingTimeLabel } = themeLocale.value
+const {
+    // readingTimeIcon,
+    readingTimeLabel
+} = themeLocale.value
 const readingTimeData = useReadingTimeData()
 const readingTimeLocale = useReadingTimeLocale();
 const { time, words } = readingTimeLocale.value
 const minutes = readingTimeData.value?.minutes
 const minsMeta = computed(() => `${minutes}M`)
-const tooltip = computed(() => `${time} ${words}`)
+// const tooltip = computed(() => `${time} ${words}`)
 </script>
 
 <template>
     <span v-if="readingTimeData" class="vp-page-info-item vp-page-info-reading-time" :aria-label="readingTimeLabel"
-        :data-tooltip="tooltip">
+        :data-tooltip="words">
         <fg-icon icon="bi-hourglass-bottom" />
         <span v-text="time" />
         <meta v-if="minsMeta" property="timeRequired" :content="minsMeta">
